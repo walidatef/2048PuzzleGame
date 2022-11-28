3481c4daa659fa4cfe88cc84f827b715538fe28c
walidatef <waliddatef@gmail.com>
2022-11-28 14:53:36 +0200

    init

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class GameController : MonoBehaviour {
+
+	//timer
+	public Text time_Text;
+	int timer = 60;
+	//end timer
+
+	void Start(){
+		StartCoroutine (Timer ());
+	}
+
+	void Update () {
+
+	
+	}
+
+	IEnumerator Timer(){
+
+		while(timer!=0){
+			if (timer< 10)
+				time_Text.text = "0"+timer;
+			else
+				time_Text.text =timer.ToString();
+			yield return new WaitForSeconds (1);
+			timer--;
+		}
+	}
+
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+	
+
+	void Update(){
+
+
+	}
+	public void PlayGame(){
+		SceneManager.LoadScene ("Game");
+	}
+
+	public void GoToMainMenu(){
+		SceneManager.LoadScene ("Menu");
+	}
+}
